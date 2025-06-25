af284b2a43d9e8dc5ea322c5a5f7ba2efdcd9a02
JoshuaKatebe <101856266+JoshuaKatebe@users.noreply.github.com>
2025-06-26 00:00:24 +0200

    Tanshi Redesign

--- src/components/SectionWrapper.jsx
+++
@@ -1,8 +0,0 @@
-export default function SectionWrapper({ children, className = '' }) {
-    return (
-      <section className={`py-12 px-6 lg:px-24 ${className}`}>
-        {children}
-      </section>
-    );
-  }
-  
